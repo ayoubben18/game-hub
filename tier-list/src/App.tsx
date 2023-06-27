402b342211a9366be84fd26845299e6d4a727047
ayoubben18 <124907713+ayoubben18@users.noreply.github.com>
2023-06-27 19:15:59 +0100

    responsive layout

---
+++ tier-list/src/App.tsx
@@ -0,0 +1,26 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="Aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        main
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
