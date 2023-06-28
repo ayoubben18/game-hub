50fc300f171120ef972619d2833ec458a04b3394
ayoubben18 <124907713+ayoubben18@users.noreply.github.com>
2023-06-28 08:27:08 +0100

    dark mode is done

--- tier-list/src/components/navbar.tsx
+++ tier-list/src/components/navbar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
+import ColorModeSwiitch from "./ColorModeSwiitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="20px">
       <Image src={logo} boxSize="60px" />
-      <Text>NavBar</Text>
+      <ColorModeSwiitch />
     </HStack>
   );
 };
