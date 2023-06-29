5fd11d8f84c20777862e64c4607615f83ab12d4a
ayoubben18 <124907713+ayoubben18@users.noreply.github.com>
2023-06-29 20:33:30 +0100

    refactor the code for extracting a query object

--- tier-list/src/components/GameGrid.tsx
+++ tier-list/src/components/GameGrid.tsx
@@ -2,16 +2,14 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "./hooks/useGames";
 import Gamecard from "./gamecard";
 import GamecardSkeleton from "./gamecardSkeleton";
-import { Genre } from "./hooks/useGenres";
-import { Platform } from "./hooks/usePlatforms";
+import { GameQuery } from "../App";
 
 interface Props {
-  selectedGenre: Genre | null;
-  selectedPlatform: Platform | null;
+  gamequery: GameQuery;
 }
 
-const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
+const GameGrid = ({ gamequery }: Props) => {
+  const { data, error, isLoading } = useGames(gamequery);
   const Skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
