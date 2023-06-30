06c0b089e22502e05f03eb666df424ce56ecfa4c
ayoubben18 <124907713+ayoubben18@users.noreply.github.com>
2023-06-30 16:34:20 +0100

    finisyou refactor

--- tier-list/src/components/GameGrid.tsx
+++ tier-list/src/components/GameGrid.tsx
@@ -12,21 +12,20 @@ const GameGrid = ({ gamequery }: Props) => {
   const { data, error, isLoading } = useGames(gamequery);
   const Skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={5}
-      >
-        {isLoading &&
-          Skeletons.map((Skeleton) => <GamecardSkeleton key={Skeleton} />)}
-        {data.map((game) => (
-          <Gamecard key={game.id} game={game} />
-        ))}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={5}
+    >
+      {isLoading &&
+        Skeletons.map((Skeleton) => <GamecardSkeleton key={Skeleton} />)}
+      {data.map((game) => (
+        <Gamecard key={game.id} game={game} />
+      ))}
+    </SimpleGrid>
   );
 };
 
