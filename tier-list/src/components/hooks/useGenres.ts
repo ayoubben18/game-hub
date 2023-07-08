47f21cb324b65f9d26d543184955d3d503fba8c4
ayoubben18 <124907713+ayoubben18@users.noreply.github.com>
2023-07-08 14:18:37 +0100

    ms

--- tier-list/src/components/hooks/useGenres.ts
+++ tier-list/src/components/hooks/useGenres.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../../api-clients";
-
+import ms from 'ms';
 export interface Genre{
     id:number;
     name: string;
@@ -13,6 +13,6 @@ const useGenres = ()=>  useQuery({
     queryKey: ['genres'],
     queryFn:
      new APIClient<Genre>('/genres').getAll,
-    staleTime:24*60*60*1000,  //24h
+    staleTime:ms('24h'),  //24h
 })
 export default useGenres;
