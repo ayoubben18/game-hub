cf13601e2481dd7aa76b743ebaca204d325d3c91
ayoubben18 <124907713+ayoubben18@users.noreply.github.com>
2023-07-11 20:15:44 +0100

    fix navbar

--- tier-list/src/components/navbar.tsx
+++ tier-list/src/components/navbar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import ColorModeSwiitch from "./ColorModeSwiitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack padding="20px">
-      <Image src={logo} boxSize="60px" />
+      <Link to="/">
+        <Image src={logo} boxSize="60px" objectFit="cover" />
+      </Link>
       <SearchInput />
       <ColorModeSwiitch />
     </HStack>
--- tier-list/src/pages/GameDetailPage.tsx
+++ tier-list/src/pages/GameDetailPage.tsx
@@ -1,9 +1,10 @@
-import { Heading, Spinner } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import useGame from "../components/hooks/useGame";
 import InformationsList from "../components/InformationsList";
 import GameTrailer from "../components/GameTrailer";
+import Gamescreenshots from "../components/Gamescreenshots";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -13,12 +14,22 @@ const GameDetailPage = () => {
   if (error || !game) throw error;
 
   return (
-    <>
-      <Heading>{game.name}</Heading>
-      <ExpandableText>{game.description_raw}</ExpandableText>
-      <InformationsList game={game} />
-      <GameTrailer gameId={game.id} />
-    </>
+    <SimpleGrid
+      columns={{
+        base: 1,
+        md: 2,
+      }}
+    >
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText>{game.description_raw}</ExpandableText>
+        <InformationsList game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id} />
+        <Gamescreenshots gameId={game.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
