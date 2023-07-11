18339e03478290f3545ac12a3e939dd4246ff525
ayoubben18 <124907713+ayoubben18@users.noreply.github.com>
2023-07-11 18:09:13 +0100

    build game attributes

--- tier-list/src/pages/GameDetailPage.tsx
+++ tier-list/src/pages/GameDetailPage.tsx
@@ -1,14 +1,12 @@
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import useGame from "../components/hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
-import { useState } from "react";
 import ExpandableText from "../components/ExpandableText";
+import useGame from "../components/hooks/useGame";
+import InformationsList from "../components/InformationsList";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
-  const [isExpanded, setIsExpanded] = useState(false);
-
   if (isLoading) return <Spinner />;
 
   if (error || !game) throw error;
@@ -17,6 +15,7 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <InformationsList game={game} />
     </>
   );
 };
